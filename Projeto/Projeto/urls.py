b546e458806b2b664950a7446a2b402e34874ce4
Eduardocesn <ceduardoadustina@gmail.com>
2023-08-19 21:05:29 -0300

    add doc swagger

--- Projeto/Projeto/urls.py
+++ Projeto/Projeto/urls.py
@@ -18,10 +18,24 @@ from django.contrib import admin
 from django.urls import path
 from Projeto import views
 from rest_framework.urlpatterns import format_suffix_patterns
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API',
+        default_version='1.0.0',
+        description='API documentation',
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', views.usuarios_list),
     path('usuarios/<int:id>', views.usuario_detalhe),
+    path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
